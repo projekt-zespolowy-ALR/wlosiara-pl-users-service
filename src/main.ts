f29c5c9fb1d5d0da2296b930d912e2dcc9edafe7
Rafał Majewski <goodheropl@gmail.com>
2023-06-13 16:51:46 +0200

    Measure communication time

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,13 @@ import AppModule from "./AppModule.js";
 import {FastifyAdapter, type NestFastifyApplication} from "@nestjs/platform-fastify";
 import AppConfig from "./app_config/AppConfig.js";
 import initializeApp from "./initializeApp.js";
+import type {FastifyReply, FastifyRequest} from "fastify";
 
 const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+app.use((req: FastifyRequest, res: FastifyReply, next: () => void) => {
+	console.log(req.method, req.url, Date.now());
+	next();
+});
 initializeApp(app);
 const appConfig = app.get(AppConfig);
 await app.listen(appConfig.PORT, "0.0.0.0");
